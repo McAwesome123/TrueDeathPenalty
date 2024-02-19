e83f1ef8227a53e430cb5463dd4c9a9c2ea9bfc4
Lexderi <adrian@rummert.de>
2024-02-19 22:06:45 +0100

    Initial commit

---
+++ TrueDeathPenalty/Patches/HUDManagerPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace TrueDeathPenalty.Patches;
+
+[HarmonyPatch(typeof(HUDManager))]
+public class HUDManagerPatch
+{
+    
+    [HarmonyPatch(nameof(HUDManager.ApplyPenalty))]
+    [HarmonyPostfix]
+    // ReSharper disable InconsistentNaming
+    public static void AdjustPenaltyTextPatch(int ___playersDead, int ___bodiesInsured, Terminal ___objectOfType, int groupCredits, EndOfGameStatUIElements ___statsUIElements)
+    // ReSharper restore InconsistentNaming
+    {
+        Plugin.LogSource.LogInfo($"players dead: {___playersDead}, bodies insured: {___bodiesInsured}, object of type: {___objectOfType}, group credits: {groupCredits}, status ui elements: {___statsUIElements}");
+        
+        int penalty = (int)((1 - (float)___objectOfType.groupCredits / groupCredits) * 100f);
+        
+        ___statsUIElements.penaltyAddition.text = $"{(object)___playersDead} casualties: -{penalty}%\n({___bodiesInsured} bodies recovered)";
+    }
+}

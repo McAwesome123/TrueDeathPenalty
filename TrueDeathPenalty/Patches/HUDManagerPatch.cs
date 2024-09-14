dcb78cfea4dc7e7b877cddf02fc312dda13447da
McAwesome <49949995+McAwesome123@users.noreply.github.com>
2024-09-14 05:44:03 +0300

    Fix division by zero if credits are zero

--- TrueDeathPenalty/Patches/HUDManagerPatch.cs
+++ TrueDeathPenalty/Patches/HUDManagerPatch.cs
@@ -7,22 +7,22 @@ namespace TrueDeathPenalty.Patches;
 public class HUDManagerPatch
 {
     private static int prevGroupCredits;
-    
+
     [HarmonyPatch("ApplyPenalty")]
     [HarmonyPrefix]
     public static void SavePreviousGroupCredits(Terminal ___terminalScript)
     {
         prevGroupCredits = ___terminalScript.groupCredits;
     }
-    
+
     [HarmonyPatch("ApplyPenalty")]
     [HarmonyPostfix]
     // ReSharper disable InconsistentNaming
     public static void AdjustPenaltyTextPatch(int playersDead, int bodiesInsured, EndOfGameStatUIElements ___statsUIElements, Terminal ___terminalScript)
     // ReSharper restore InconsistentNaming
     {
-        int penalty =  (int)Math.Round((1 - (float)___terminalScript.groupCredits / prevGroupCredits ) * 100f);
-        
+        int penalty =  (int)Math.Round((1 - (float)___terminalScript.groupCredits / Math.Max(prevGroupCredits, 1) ) * 100f);
+
         ___statsUIElements.penaltyAddition.text = $"{playersDead} casualties: -{penalty}%\n({bodiesInsured} bodies recovered)";
     }
 }
